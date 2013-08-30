e7ac6aa007053be1bc5536a4d2b4fd1abe1c178a
Wellington Fan <winterbeef@gmail.com>
2013-08-29 20:26:45 -0700

    WIP: Got it! syncing tips with sliders.

--- js/jqui-sliders.js
+++ js/jqui-sliders.js
@@ -1,55 +1,68 @@
 $(function() {
 
-	function newQtipValue(handle, newVal) {
-		$(handle).qtip('option', 'content.text', '' + newVal);
+	function setTipVal(handle, newVal) {
+		var api = $(handle).qtip('api');
+		api.set('content.text', newVal);
 	}
 
+	function qtipDefault(handle, value) {
+		return {
+			'content' : {
+				'text' : value
+			},
+			'position': {
+				'my': 'bottom center',
+				'at': 'top center',
+				'container': handle
+			},
+			'hide': {
+				'delay': 1000
+			},
+			'style': {
+				'classes': 'qtip-custom qtip-dark qtip-shadow qtip-rounded'
+			}
+		}
+	}
 
-	$(".axis-value" ).each(function() {
-		// Create the slider
-		var value = parseInt($(this).text(), 10);
-		$(this).empty().slider({
+	function makeQtipFromXv(xv, value) {
+		handle = xv.find('.ui-slider-handle')
+		handle.qtip(qtipDefault(handle, value))
+	}
+
+	function initSlider(xv, value) {
+		xv.empty().slider({
 			'value' : value,
 			'min'   : 0,
 			'max'   : 10,
 			'step'  : 1,
 			'change' : function(event, ui) {
-				newQtipValue(ui.handle, ui.value);
+				setTipVal(ui.handle, ui.value);
 			},
 			'slide' : function(event, ui) {
-				newQtipValue(ui.handle, ui.value);
+				setTipVal(ui.handle, ui.value);
 			},
 			'create': function(event, ui) {
-				// Create the qtip
-				$(this).find('.ui-slider-handle').qtip({
-					'content' : {
-						'text' : value
-					},
-					'position': {
-						'my': 'bottom center',
-						'at': 'top center',
-						'container': $(this).find('.ui-slider-handle')
-					},
-					'hide': {
-						'delay': 1000
-					},
-					'style': {
-						'classes': 'qtip-custom qtip-dark qtip-shadow qtip-rounded'
-					}
-				})
+				makeQtipFromXv(xv, value)
 			}
 		});
+	}
+
+	$(".axis-value" ).each(function() {
+		var value = parseInt($(this).text(), 10);
+		initSlider($(this), value);
 	});
 
 
-	$('.axis').on('click', '.axis-label', function (event){
-		$tr = $(event.delegateTarget);
-		$label = $(event.target);
-		$widget = $tr.find('.axis-value').slider('widget');
-		$tt_handle = $tr.find('.ui-slider-handle');
+	$('.axis').on('click', '.axis-label', function (event) {
+		label = $(event.target);
+		xv = $(event.delegateTarget).find('.axis-value');
+		widget = xv.slider('widget');
+		handle = xv.find('.ui-slider-handle');
 
-		newVal = $widget.slider('value') + ($label.hasClass('left') ? -1 : +1);
-		$widget.slider('value', newVal);
+		newVal = widget.slider('value') + (label.hasClass('left') ? -1 : +1);
+		widget.slider('value', newVal);
+		makeQtipFromXv(xv, newVal);
+		// handle.qtip(qtipDefault(handle, newVal))
 	})
 
 
