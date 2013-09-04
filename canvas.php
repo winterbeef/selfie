7a9bd4aaa6c4729a6401e79d84bf2dc639f25f91
Wellington Fan <winterbeef@gmail.com>
2013-09-04 06:22:52 -0700

    WIP: Labels as images, lines centered.

--- canvas.php
+++ canvas.php
@@ -23,14 +23,20 @@
         ?>
         </pre>
       </div><!-- /.span12 -->
-
-
       <div class="span12">
-        <canvas id="frame" width="800" height="800"></canvas >
+        <canvas id="frame" width="1000" height="1000"></canvas>
+        <br>
+      </div><!-- /.span12 -->
+      <div class="span12">
         <button id="dl" class="btn btn-large btn-primary" type="button">Download</button>
+        <br><br>
       </div><!-- /.span12 -->
     </div><!-- /.row -->
   </div><!-- /.container -->
+
+  <?php foreach (range(1,64) as $i) :?>
+  <img class="axis-image" id="a_<?=sprintf("%02d", $i)?>" src="/whoami/images/whoami_<?=sprintf("%02d", $i)?>.png">
+  <?php endforeach; ?>
 
   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
   <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
@@ -39,61 +45,67 @@
   <script type="text/javascript" src="/whoami/js/fabric-custom.js"></script>
   <script type="text/javascript" src="/whoami/js/data.php"></script>
     <script type="text/javascript">
-    $(function() {
+    $(window).load(function() {
         var canvas = new fabric.Canvas('frame');
 
         var axes = <?= json_encode(getAxes()); ?>
 
         var me = <?= json_encode(getMe($_GET['me'])); ?>
 
-        var label = 200;
-        var pad   = 8;
-        var h = $('#frame').height();
-        var w = $('#frame').width();
-        var fontSize = 14;
-        var wedge = (180.0/axes.length);
+        var image = 200;                   // Width of label image.
+        var label = 240;                   // Width reserved for label. Image
+                                           // must be smaller than this.
+        var pad   = 0;                     // Between label and line
+        var h = $('#frame').height();      // Extent of canvas
+        var w = $('#frame').width();       // Extent of canvas
+        var theta = (180.0/axes.length);   // Angle of each wedge
 
-        var Lconfig = {
-          fontSize: fontSize,
-          left    : label,
-          top     : h/2,
-          originX :'right',
-          originY :'center'
-        }
-        var Rconfig = {
-          fontSize: fontSize,
-          left    : (w-label),
-          top     : h/2,
-          originX :'left',
-          originY :'center'
-        }
-        var lineConfig = {
-          fill       : 'black',
+        var params = {
           stroke     : 'black',
           strokeWidth: 1,
           selectable: false
         }
 
+        var images = new Array();
+        $('.axis-image').each(function(i, img) {
+          var canvasImage = new fabric.Image(img, {top : h/2, originX: 'left'});
+          images[images.length] = canvasImage
+          $(img).remove();
+        })
+
         $.each(axes, function(i, axis) {
-            var Left  = new fabric.Text(axis.left,  Lconfig);
-            var Right = new fabric.Text(axis.right, Rconfig);
-            var angle = i * wedge - 90;
+            var angle = i * theta - 90;
+            params.stroke = axis.color;
 
-            lineConfig.fill = lineConfig.stroke = axis.color;
-            lineConfig.angle = angle;
-            var Line = new fabric.Line([0+pad+label, h/2, w-pad-label, h/2], lineConfig);
+            var line = new fabric.Line([0+pad+label, h/2, w-pad-label, h/2], params);
+            var imageL = images[Math.floor(i*2+0)];
+            var imageR = images[Math.floor(i*2+1)];
 
-            var group = new fabric.Group([Left, Right], {
+            imageL.left = label-image;
+            imageR.left = w-label;
+
+            if (console && console.log ) {
+              console.log( imageL.left, imageR.left);
+            }
+
+            var group = new fabric.Group([imageL, imageR, line], {
                 angle: angle,
                 selectable: false
             })
-
-
-            console.log( i, group.getCenterPoint().x);
-
-            canvas.add(Line)
             canvas.add(group)
         })
+
+
+
+
+
+
+
+
+
+
+
+
 
         $('#dl').click(function(e){
             window.open(canvas.toDataURL({format:'png'}))
